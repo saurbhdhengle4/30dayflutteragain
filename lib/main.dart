8e5a888c2033ff2798ce90258053130a181f6ae0
unknown <saurbhdhengle4@gmail.com>
2021-12-14 16:15:16 +0530

    day5 done

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,29 @@
+import 'package:catalog/Pages/Home_Page.dart';
+import 'package:catalog/Pages/loginpage.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Material(
-      child: Center(
-        child: Container(
-          child: Text("welcom to 30 day flutter"),
-        ),
-      ),
-    ));
+      // home: HomePsge(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+          fontFamily: GoogleFonts.lato().fontFamily,
+          // primaryTextTheme: GoogleFonts.latoTextTheme(),
+          primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      initialRoute: "/",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePsge(),
+        "/login": (context) => LoginPage(),
+      },
+    );
   }
 }
